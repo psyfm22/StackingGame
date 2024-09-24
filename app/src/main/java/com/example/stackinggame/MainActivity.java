de5a458fc9c9b8a81d96c82d6cf5b6222f3c4363
Freddie Moir <freddiemoir@icloud.com>
2024-09-24 17:08:43 +0100

    Box now moves down when button pressed and gets a location

--- app/src/main/java/com/example/stackinggame/MainActivity.java
+++ app/src/main/java/com/example/stackinggame/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.stackinggame;
 import android.animation.Animator;
 import android.animation.ObjectAnimator;
 import android.os.Bundle;
+import android.util.Log;
 import android.util.TypedValue;
 import android.view.View;
 import android.widget.Button;
@@ -15,6 +16,7 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
     ObjectAnimator animator;
+    ObjectAnimator animator2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
 
         View movingBox = findViewById(R.id.movingBox);
         Button button = findViewById(R.id.button);
+        Button button2 = findViewById(R.id.button2);
 
         float boxWidthInPx = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 174, getResources().getDisplayMetrics());
 
@@ -36,6 +39,14 @@ public class MainActivity extends AppCompatActivity {
             animator.start();
         });
 
+        button2.setOnClickListener(view -> {
+            animator.pause();
+            Log.d("COMP3018", ""+ movingBox.getX());
+            animator2 = ObjectAnimator.ofFloat(movingBox, "translationY", 0f, movingBox.getX());
+            animator2.setDuration(3000);
+            animator2.start();
+        });
+
         animator.addListener(new Animator.AnimatorListener() {
             @Override
             public void onAnimationStart(Animator animation) {}
@@ -47,7 +58,7 @@ public class MainActivity extends AppCompatActivity {
                 startingLocation[0] = endingLocation[0];
                 endingLocation[0] = holder;
                 animator.setFloatValues(startingLocation[0], endingLocation[0]);
-                animator.start(); // Restart the animation
+                animator.start();
             }
 
             @Override
