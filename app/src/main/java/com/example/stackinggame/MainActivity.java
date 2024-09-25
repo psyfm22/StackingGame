9fd2ee87f4f5bf2f1940efb61b4ef865e980ccc7
Freddie Moir <freddiemoir@icloud.com>
2024-09-25 12:32:21 +0100

    Now use hotel images and it lands on top

--- app/src/main/java/com/example/stackinggame/MainActivity.java
+++ app/src/main/java/com/example/stackinggame/MainActivity.java
@@ -7,6 +7,7 @@ import android.util.Log;
 import android.util.TypedValue;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -23,18 +24,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        View movingBox = findViewById(R.id.movingBox);
+
         Button button = findViewById(R.id.button);
         Button button2 = findViewById(R.id.button2);
 
-        float boxWidthInPx = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 174, getResources().getDisplayMetrics());
-        float boxHeightInPx = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 174, getResources().getDisplayMetrics());
+        ImageView hotelFloor = findViewById(R.id.hotelMiddleIV);
+
+
+        float boxWidthInPx = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 600, getResources().getDisplayMetrics());
+        float boxHeightInPx = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 216, getResources().getDisplayMetrics());
+        float upperConstraint = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 175, getResources().getDisplayMetrics());
 
         final float[] startingLocationX = {0f};
         final float[] endingLocationX = {getResources().getDisplayMetrics().widthPixels - boxWidthInPx};
-        final float[] endingLocationY = {getResources().getDisplayMetrics().heightPixels - boxHeightInPx - boxWidthInPx};
+        final float[] endingLocationY = {getResources().getDisplayMetrics().heightPixels - upperConstraint - boxHeightInPx - boxHeightInPx};
 
-        animator = ObjectAnimator.ofFloat(movingBox, "translationX", startingLocationX[0], endingLocationX[0]);
+        animator = ObjectAnimator.ofFloat(hotelFloor, "translationX", startingLocationX[0], endingLocationX[0]);
         animator.setDuration(3000);
 
         button.setOnClickListener(view -> {
@@ -43,8 +48,8 @@ public class MainActivity extends AppCompatActivity {
 
         button2.setOnClickListener(view -> {
             animator.pause();
-            Log.d("COMP3018", ""+ movingBox.getX());
-            animator2 = ObjectAnimator.ofFloat(movingBox, "translationY", 0f, endingLocationY[0]);
+            Log.d("COMP3018", ""+ hotelFloor.getX());
+            animator2 = ObjectAnimator.ofFloat(hotelFloor, "translationY", 0f, endingLocationY[0]);
             animator2.setDuration(3000);
             animator2.start();
         });
