2ac995e6ea7e153963888da29bd30d5962ad06a3
Freddie Moir <freddiemoir@icloud.com>
2024-09-25 15:52:43 +0100

    Now works with multiple images

--- app/src/main/java/com/example/stackinggame/MainActivity.java
+++ app/src/main/java/com/example/stackinggame/MainActivity.java
@@ -4,67 +4,85 @@ import android.animation.Animator;
 import android.animation.ObjectAnimator;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.util.Log;
 import android.util.TypedValue;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.constraintlayout.widget.ConstraintSet;
 
 public class MainActivity extends AppCompatActivity {
     private ObjectAnimator animatorX;
-    private ObjectAnimator animator2;
+    private ObjectAnimator animatorY;
     private int hotelCountStackingActivity;
-    ConstraintLayout layout;
-    ImageView hotelMiddleIV;
+    private ConstraintLayout constraintLayout;
+    private ImageView hotelMiddleIV;
+    private float startingLocationX, endingLocationX, endingLocationY;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        layout = findViewById(R.id.main);
-
         SharedPreferences sharedPreferences = getSharedPreferences("SharedPreferences", MODE_PRIVATE);
         hotelCountStackingActivity = sharedPreferences.getInt("hotelCountStackingActivity", 0);
-
-
-        Button button = findViewById(R.id.button);
-        Button button2 = findViewById(R.id.button2);
-
-        hotelMiddleIV = findViewById(R.id.hotelMiddleIV);
-
 
         float boxWidthInPx = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 600, getResources().getDisplayMetrics());
         float boxHeightInPx = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 216, getResources().getDisplayMetrics());
         float upperConstraint = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 175, getResources().getDisplayMetrics());
 
-        final float[] startingLocationX = {0f};
-        final float[] endingLocationX = {getResources().getDisplayMetrics().widthPixels - boxWidthInPx};
-        final float[] endingLocationY = {getResources().getDisplayMetrics().heightPixels - upperConstraint - boxHeightInPx - boxHeightInPx};
+        startingLocationX = 0f;
+        endingLocationX = getResources().getDisplayMetrics().widthPixels - boxWidthInPx;
+        endingLocationY = getResources().getDisplayMetrics().heightPixels - upperConstraint - boxHeightInPx - boxHeightInPx;
 
-        animatorX = ObjectAnimator.ofFloat(hotelFloor, "translationX", startingLocationX[0], endingLocationX[0]);
+        constraintLayout = findViewById(R.id.main);
+        Button addNewHotelLayerButton = findViewById(R.id.button);
+        Button placeHotelLayerButton = findViewById(R.id.button2);
+        hotelMiddleIV = findViewById(R.id.hotelMiddleIV);
+
+
+
+        addNewHotelLayerButton.setOnClickListener(view -> {
+            addNewImage();
+        });
+
+        placeHotelLayerButton.setOnClickListener(view -> {
+            if(animatorX!=null){
+                animatorX.pause();
+                animatorY = ObjectAnimator.ofFloat(hotelMiddleIV, "translationY", 0f, endingLocationY);
+                animatorY.setDuration(3000);
+                animatorY.start();
+            }
+        });
+    }
+
+    private void addNewImage(){
+        hotelMiddleIV = new ImageView(this);
+
+        hotelMiddleIV.setId(View.generateViewId());
+        hotelMiddleIV.setImageResource(R.drawable.apartment_window);
+
+        ConstraintLayout.LayoutParams layoutParams = new ConstraintLayout.LayoutParams(
+                (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 600, getResources().getDisplayMetrics()),
+                (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 216, getResources().getDisplayMetrics())
+        );
+
+        hotelMiddleIV.setLayoutParams(layoutParams);
+        constraintLayout.addView(hotelMiddleIV);
+
+        ConstraintSet constraintSet = new ConstraintSet();
+        constraintSet.clone(constraintLayout);
+
+        constraintSet.connect(hotelMiddleIV.getId(), ConstraintSet.TOP, constraintLayout.getId(), ConstraintSet.TOP, 120);
+        constraintSet.connect(hotelMiddleIV.getId(), ConstraintSet.LEFT, constraintLayout.getId(), ConstraintSet.LEFT, 0);
+
+        constraintSet.applyTo(constraintLayout);
+
+        animatorX = ObjectAnimator.ofFloat(hotelMiddleIV, "translationX", startingLocationX, endingLocationX);
         animatorX.setDuration(3000);
-
-        button.setOnClickListener(view -> {
-//            animatorX.start();
-        });
-
-        button2.setOnClickListener(view -> {
-//            animatorX.pause();
-            addNewFloor();
-
-//            animator2 = ObjectAnimator.ofFloat(hotelFloor, "translationY", 0f, endingLocationY[0]);
-//            animator2.setDuration(3000);
-//            animator2.start();
-        });
+        animatorX.start();
 
         animatorX.addListener(new Animator.AnimatorListener() {
             @Override
@@ -73,10 +91,10 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onAnimationEnd(Animator animation) {
                 float holder;
-                holder = startingLocationX[0];
-                startingLocationX[0] = endingLocationX[0];
-                endingLocationX[0] = holder;
-                animatorX.setFloatValues(startingLocationX[0], endingLocationX[0]);
+                holder = startingLocationX;
+                startingLocationX = endingLocationX;
+                endingLocationX = holder;
+                animatorX.setFloatValues(startingLocationX, endingLocationX);
                 animatorX.start();
             }
 
@@ -86,28 +104,5 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onAnimationRepeat(Animator animation) {}
         });
-    }
-
-    private void addNewFloor(){
-        hotelCountStackingActivity++;
-
-        ImageView newFloor = new ImageView(this);
-        newFloor.setImageResource(R.drawable.apartment_window);
-//
-//        float width =  TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 600, getResources().getDisplayMetrics());
-//        float height =  TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 216, getResources().getDisplayMetrics());
-//
-//        ConstraintLayout.LayoutParams layoutParams = new ConstraintLayout.LayoutParams((int) width, (int) height);
-//
-//        // Set constraints for the new ImageView
-//        layoutParams.startToStart = ConstraintLayout.LayoutParams.PARENT_ID;
-//        layoutParams.endToEnd = ConstraintLayout.LayoutParams.PARENT_ID;
-//        layoutParams.topToTop = ConstraintLayout.LayoutParams.PARENT_ID; // Adjust this as needed
-//        layoutParams.bottomToBottom = ConstraintLayout.LayoutParams.UNSET; // Adjust this to position the new floor as needed
-//
-//        newFloor.setLayoutParams(layoutParams);
-//        newFloor.setImageResource(R.drawable.apartment_window);
-//
-//        layout.addView(newFloor, 0);
     }
 }
